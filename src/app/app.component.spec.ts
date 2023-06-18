4503fbff2e3deab597d3ae270b0dea923b0c0456
partharanjan <partharanjan.nayak@gmail.com>
2023-06-18 17:43:08 +0530

    test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'swiggy-clone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('swiggy-clone');
+    expect(app.title).toEqual('swiggy clone v2');
   });
 
   it('should render title', () => {
